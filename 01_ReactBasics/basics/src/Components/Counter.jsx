2ef7f92689158599cc99949436dd50117730726b
meetmmpatel <meetmmpatel@gmail.com>
2020-05-15 13:37:36 -0400

    Complete the counter

--- 01_ReactBasics/basics/src/Components/Counter.jsx
+++ 01_ReactBasics/basics/src/Components/Counter.jsx
@@ -9,6 +9,8 @@ export default class Counter extends Component {
   }
 
   incrementCount = () => this.setState((preNum) => ({ count: preNum.count++ }));
+  decrementCount = () => this.setState((preNum) => ({ count: preNum.count-- }));
+  resetCount = () => this.setState(() => ({ count: 0 }));
 
   render() {
     return (
@@ -19,8 +21,12 @@ export default class Counter extends Component {
             <button onClick={this.incrementCount} className="btn btn-success">
               Add Counter
             </button>
-            <button className="btn btn-danger">Reset Counter</button>
-            <button className="btn btn-success">Reduce Counter</button>
+            <button onClick={this.resetCount} className="btn btn-danger">
+              Reset Counter
+            </button>
+            <button onClick={this.decrementCount} className="btn btn-success">
+              Reduce Counter
+            </button>
           </div>
         </div>
       </div>
