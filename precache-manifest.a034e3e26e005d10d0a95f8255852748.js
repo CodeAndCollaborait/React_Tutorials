e6eac504e05f5c38dcec27ed32aa5dff3534f2b3
meetmmpatel <meetmmpatel@gmail.com>
2020-05-26 22:05:25 -0400

    Updates

---
+++ precache-manifest.a034e3e26e005d10d0a95f8255852748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c142b5ce750da09ba71cbc8f4cd9718a",
+    "url": "/React_Tutorials/index.html"
+  },
+  {
+    "revision": "52b3db5a1b858d6e23d7",
+    "url": "/React_Tutorials/static/css/main.d513d9e5.chunk.css"
+  },
+  {
+    "revision": "760bb2b41b69c3530b70",
+    "url": "/React_Tutorials/static/js/2.82917e50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Tutorials/static/js/2.82917e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b3db5a1b858d6e23d7",
+    "url": "/React_Tutorials/static/js/main.a1cc0418.chunk.js"
+  },
+  {
+    "revision": "65968bb5046940af2700",
+    "url": "/React_Tutorials/static/js/runtime-main.83e2969c.js"
+  }
+]);
